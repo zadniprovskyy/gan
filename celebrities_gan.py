da2f3276a39961585d0c48b438994295a790ce6c
Yegor <yegor.zadniprovskyy@mail.utoronto.ca>
2019-08-05 19:34:07 -0400

    Commented out main part in celebrities.

--- celebrities_gan.py
+++ celebrities_gan.py
@@ -28,19 +28,19 @@ def load_and_preprocess_image(path):
     image = tf.io.read_file(path)
     return preprocess_image(image)
 
-
-if __name__=='__main__':
-    dataset = load_celebrities_dataset()
-
-
-    train_dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
-
-    # Plot input images
-    # for image_batch in train_images:
-    #     plt.imshow((image_batch * 127.5 + 127.5)[0, :, :, 0], cmap='gray')
-
-
-    gan = GAN()
-    # set batch size before training
-    gan.set_batch_size(batch_size=BATCH_SIZE)
-    gan.train(train_dataset, EPOCHS)
+#
+# if __name__=='__main__':
+#     dataset = load_celebrities_dataset()
+#
+#
+#     train_dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+#
+#     # Plot input images
+#     # for image_batch in train_images:
+#     #     plt.imshow((image_batch * 127.5 + 127.5)[0, :, :, 0], cmap='gray')
+#
+#
+#     gan = GAN()
+#     # set batch size before training
+#     gan.set_batch_size(batch_size=BATCH_SIZE)
+#     gan.train(train_dataset, EPOCHS)
