c03789660356c10a47551500386138032dbb4e98
Yegor <yegor.zadniprovskyy@mail.utoronto.ca>
2019-08-07 16:05:38 -0400

    Fixed per-epoch image saving.

--- gan.py
+++ gan.py
@@ -111,20 +111,21 @@ class GAN():
     def generate_and_save_images(self, model, epoch, test_input):
       # Notice `training` is set to False.
       # This is so all layers run in inference mode (batchnorm).
-      predictions = model(test_input, training=False)
+        predictions = model(test_input, training=False)
 
-      fig = plt.figure(figsize=(4,4))
+        fig = plt.figure(figsize=(4,4))
 
-      for i in range(predictions.shape[0]):
-          plt.subplot(4, 4, i+1)
+        for i in range(predictions.shape[0]):
+            plt.subplot(4, 4, i+1)
+            # if deal with only one colour channel
+            if self.gen_input_shape[-1] == 1:
+                plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+            elif self.gen_input_shape[-1] == 3:
+                plt.imshow(tf.dtypes.cast(x=(predictions[i] * 127.5 + 127.5), dtype=tf.uint32))
+        plt.axis('off')
 
-          # if deal with only one colour channel
-          if self.gen_input_shape[-1] == 1:
-            plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-          plt.axis('off')
-
-      plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-      plt.show()
+        plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+        plt.show()
 
     # Notice the use of `tf.function`
     # This annotation causes the function to be "compiled".
